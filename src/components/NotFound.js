f28724df109f4abf0997f0a3f813dd5eeba55118
Jonas Antunes <jonas.lima@grupogaia.com.br>
2020-08-02 14:43:24 -0300

    404 component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import AvalonLogo from "./AvalonLogo";
 
 const NotFound = () => {
-    return (
+  return (
+    <>
+      <div className="not-found">
         <h1>
-            404
+          Tentamos, mas não conseguimos encontrar o que você procura. <small>(Error 404)</small>
         </h1>
-    )
-}
+        <h2>Que tal tentarmos do início?</h2>
+        <AvalonLogo color={"#ff2ea2"} ratio={0.2} />
+      </div>
+      <div className="not-found__background"></div>
+    </>
+  );
+};
 
-export default NotFound
+export default NotFound;
